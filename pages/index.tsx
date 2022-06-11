e56d68d828928ac68f6673e2358fd960cf195c1a
Mohammad Sahil <sahilmejakhas@gmail.com>
2022-06-11 19:46:52 +0530

    dark mode added

--- pages/index.tsx
+++ pages/index.tsx
@@ -34,7 +34,7 @@ const items = [
 const Home = () => {
   const user = useStore(state =>  state.user)
   const [ toggleDarkModee, setToggleDarkMode] = useState(false)
-  const toggleDarkMode = useStore(state =>  state.toggleDarkMode)
+  const toggleDarkMode = useStore((state: { toggleDarkMode: any; }) =>  state.toggleDarkMode)
   const dark = useStore(state =>  state.dark)
   const [collapsed, setCollapsed] = useState(false);
   useEffect(() => {
