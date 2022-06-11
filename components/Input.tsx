ab8fff318cb45be428078f25e449d0452383bc32
Mohammad Sahil <sahilmejakhas@gmail.com>
2022-06-11 11:50:36 +0530

    zustand issue

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,10 +1,11 @@
 import useStore from '../zustand/store'
 import { useState } from 'react'
+// import Input2 from './Input2'
 
 const Input = () => {
 // const inputRef = useRef()
 const [ input,  setInput ] = useState(null)
-const userData = useStore(state =>  state.userData)
+const userData = useStore((state: { userData: any }) =>  state.userData)
 
 const addPerson = () => {
     userData(input)
@@ -14,6 +15,7 @@ const addPerson = () => {
   return (
     <form onSubmit={e =>  e.preventDefault()}>
         <input type='text' value={input} onChange={(e) => setInput(e.target.value)} />
+        {/* <Input2/> */}
         <button onClick={addPerson}>Addd person</button>
     </form>
   )
--- zustand/store.tsx
+++ zustand/store.tsx
@@ -1,10 +1,10 @@
 import create from 'zustand'
 import { devtools, persist } from 'zustand/middleware'
 
-const store = devtools((set) => ({
+const store = (set: (arg0: (state: any) => { user: any[] }) => any) => ({
   user: ['Sahil', 'Male',  29],
-  userData: (data) => set(state => ({ user: [...state.user, data] })),
-}))
+  userData: (data: any) => set(state => ({ user: [...state.user, data] })),
+})
 // store = persist(store, {name:  'user_settings'})
 
 const useStore = create(store)
